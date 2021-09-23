a6d8e1cbe84a3a41888abba34e5549be2ed49b3c
zhangke <1826213722@qq.com>
2021-09-23 21:34:36 +0800

    初始化java-basic项目

---
+++ java-basic/src/main/java/io/github/ziqicongdonglai/java/basic/App.java
@@ -0,0 +1,18 @@
+package io.github.ziqicongdonglai.java.basic;
+
+import io.github.ziqicongdonglai.java.basic.array.ArrayDemo;
+
+/**
+ * @author ke_zhang
+ * @create 2021-09-23 21:25
+ * @description
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("app launch");
+        int[] arr = {1, 2, 3, 4, 5};
+        ArrayDemo ad = new ArrayDemo();
+        int sum = ad.getSum(arr);
+        System.out.println("sum = " + sum);
+    }
+}
---
+++ java-basic/src/main/java/io/github/ziqicongdonglai/java/basic/array/ArrayDemo.java
@@ -0,0 +1,27 @@
+package io.github.ziqicongdonglai.java.basic.array;
+
+/**
+ * @author ke_zhang
+ * @create 2021-09-23 21:27
+ * @description 数组练习
+ */
+public class ArrayDemo {
+    /**
+     * 求数组元素之和
+     *
+     * @param array 整型形参数组
+     * @return int：元素之和
+     */
+    public int getSum(int[] array) {
+        // sum为累加器，初始化为0
+        int sum = 0;
+        /*
+         以下通过循环遍历数组，累加求和
+         */
+        for (int i = 0; i < array.length; i++) {
+            sum += i;
+        }
+        // 返回sum
+        return sum;
+    }
+}
