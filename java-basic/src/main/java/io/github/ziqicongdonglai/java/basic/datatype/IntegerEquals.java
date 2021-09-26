f11e3e62dfc59029cc2526352c162028ff938b11
zhangke <1826213722@qq.com>
2021-09-26 23:01:06 +0800

    day4 datatype

---
+++ java-basic/src/main/java/io/github/ziqicongdonglai/java/basic/datatype/IntegerEquals.java
@@ -0,0 +1,6 @@
+package io.github.ziqicongdonglai.java.basic.datatype;/**
+* @author ke_zhang
+* @create 2021-09-26 21:09
+* @description
+*/public class IntegerEquals {
+}
---
+++ java-basic/src/main/java/io/github/ziqicongdonglai/java/basic/datatype/StringEquals.java
@@ -0,0 +1,6 @@
+package io.github.ziqicongdonglai.java.basic.datatype;/**
+* @author ke_zhang
+* @create 2021-09-26 21:10
+* @description
+*/public class StringEquals {
+}
